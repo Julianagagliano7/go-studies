eca7d4e4ea3db8f93ba7c3ce74f0d160f3846ede
Julianagagliano7 <julianagagliano1@gmail.com>
2025-05-18 20:14:25 -0300

    bool

--- main.go
+++ main.go
@@ -3,12 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var floatNumber float32 = 1.1 //32 bits - apenas operações matemáticas do mesmo tipo de float são permitidas
+	 var maior bool = 10 > 5
+	 var menor bool = 10 < 5
 
-	var pi float64 = 3.14159
-	var radius float64 = 5.0
-	var area float64 = pi * radius * radius
-
-	fmt.Println("Area of the circle:", area)
-	fmt.Println("Float number:", floatNumber)
+	 fmt.Println(maior)
+	 fmt.Println(menor)
 }
