d07c05a09de546c8537531bddc1d64f0101f6be8
Julianagagliano7 <julianagagliano1@gmail.com>
2025-05-18 20:01:22 -0300

    int config for better memory handling

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
-const name = "Juliana"
-
 func main() {
-	age := 21
+	var idade int = 30   //ocupa tamanho da memória do sistema, padrão 64 bits
+	var idade1 int32 = 2 //ocupa 32 bits
+	var idade2 int8 = 1 //ocupa 8 bits
 
-	fmt.Println("My name is", name + " and I am", age, "years old.") 
+	fmt.Println(idade)
+	fmt.Println(idade1)
+	fmt.Println(idade2)
 }
 
