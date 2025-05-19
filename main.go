149a4dabf23bea058e1d61bf401e8f2864153cd7
Julianagagliano7 <julianagagliano1@gmail.com>
2025-05-19 14:57:15 -0300

    pointers

--- main.go
+++ main.go
@@ -3,24 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	a := 10
+	variavel := 10
 
-	var pointer *int = &a //recupera o enderço de memória da variável a
+	fmt.Println(variavel)
 
-	fmt.Println(pointer)
-	fmt.Println(*pointer) //recupera o valor armazenado no endereço de memória
+	abc(&variavel)
 
-	*pointer = 50
+	fmt.Println(variavel)
+}
 
-	fmt.Println(*pointer)
-	fmt.Println(pointer)
-	fmt.Println(a)
-
-	b := &a 
-	*b = 20
-
-	// 3 variáveis interligadas pelo apontamento ao mesmo endereço de memória 
-	fmt.Println(*b)
-	fmt.Println(*pointer)
-	fmt.Println(a)
+func abc(a *int) {
+	*a = 200
 }
