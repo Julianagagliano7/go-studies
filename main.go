885de7c50d2de3734575e87e14f9c6f29b19b530
Julianagagliano7 <julianagagliano1@gmail.com>
2025-05-19 14:35:04 -0300

    pointers - test

--- main.go
+++ main.go
@@ -9,4 +9,26 @@ func main() {
 
 	fmt.Println(pointer)
 	fmt.Println(*pointer) //recupera o valor armazenado no endereço de memória
+
+	*pointer = 50
+
+	fmt.Println(*pointer)
+	fmt.Println(pointer)
+	fmt.Println(a)
+
+	b := &a 
+	*b = 20
+
+	// 3 variáveis interligadas pelo apontamento ao mesmo endereço de memória 
+	fmt.Println(*b)
+	fmt.Println(*pointer)
+	fmt.Println(a)
+
+
+
+
+
+
+
+
 }
