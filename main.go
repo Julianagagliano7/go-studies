da7e2081d7c46c25578ddb1ecc4cccab1e39cbfc
Julianagagliano7 <julianagagliano1@gmail.com>
2025-06-03 07:55:58 -0300

    slice

--- main.go
+++ main.go
@@ -2,11 +2,27 @@ package main
 
 import "fmt"
 
+//slice possuem tamanho flexível (dinâmico)
+//armazenam dados do mesmo tipo
+
+//inserção: uso a função append (aumenta o slice por baixo dos panos), insere sempre no final
+
 func main() {
-	var gavetas [2]string
-	gavetas[0] = "Chaves"
-	gavetas[1] = "Documentos"
+	var gavetas []string
 
-	fmt.Println(gavetas[1])
+	gavetas = append(gavetas, "copos", "panos", "colheres") //inserção
+	fmt.Println(gavetas[2])                                 //leitura
 
+	fmt.Println(len(gavetas))
+
+	// Divisão de slice
+	// slice[x:x-1] -> último x é sempre o len do slice - 1
+
+	fmt.Println(gavetas[0:2])
+
+
+	//Remoção de itens do slice 
+
+	gavetas = gavetas[:2] 
+	fmt.Println(gavetas) 
 }
