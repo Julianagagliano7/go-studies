8a7d84ba64dba2e1b4f4235b8b541a5d24054dd7
Julianagagliano7 <julianagagliano1@gmail.com>
2025-06-09 14:58:53 -0300

    pointers-study

--- main.go
+++ main.go
@@ -7,20 +7,25 @@ func main() {
 
 	var pointer *int = &a //recupera o enderço de memória da variável a
 
-	fmt.Println(pointer)
-	fmt.Println(*pointer) //recupera o valor armazenado no endereço de memória
+	// fmt.Println(&a)       
+	// fmt.Println(pointer)
+	// fmt.Println(*pointer) //recupera o valor armazenado no endereço de memória
 
 	*pointer = 50
 
-	fmt.Println(*pointer)
-	fmt.Println(pointer)
-	fmt.Println(a)
+	// fmt.Println(*pointer)
+	// fmt.Println(pointer)
+	// fmt.Println(&a)
+	// fmt.Println(a)
 
 	b := &a 
 	*b = 20
 
-	// 3 variáveis interligadas pelo apontamento ao mesmo endereço de memória 
-	fmt.Println(*b)
-	fmt.Println(*pointer)
+	fmt.Println(b)
+	fmt.Println(pointer)
+	fmt.Println(&a)
+
 	fmt.Println(a)
+	fmt.Println(*pointer)
+	fmt.Println(*b)
 }
