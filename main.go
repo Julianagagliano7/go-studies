070548707db043b1e5531fefd796173a7a7781bd
Julianagagliano7 <julianagagliano1@gmail.com>
2025-07-07 08:35:37 -0300

    range

--- main.go
+++ main.go
@@ -2,37 +2,23 @@ package main
 
 import "fmt"
 
-//for inicializacao; condicao; fim iteracao{
-
-//}
+//range acessa o valor do elemento com mais facilidade
 
 func main() {
+	//Exemplo com slice
+	// nums := []string{"pedro", "caio", "juliana"}
 
-	//For comum
-	// sum := 0
-
-	// for i := 0; i < 10; i++ {
-	// 	fmt.Println(i)
-	// 	sum += i
+	// for key, value := range nums {
+	// 	fmt.Println(key, value)
 	// }
 
-	// fmt.Println(sum)
-
-	//Simulando o while
-	// sum := 0
-
-	// for sum < 20 {
-	// 	fmt.Println("loop")
-	// 	sum += 2
-	// }
-
-	// fmt.Println(sum)
-
-	//For de Slices
-	nums := []int{1, 2, 3, 4, 5}
-
-	for i := 0; i < len(nums); i++ {
-		fmt.Println(nums[i])
+	//Exemplo com map
+	users := map[string]string{
+		"nome":      "João",
+		"documento": "CPF",
 	}
 
+	for key, value := range users {
+		fmt.Println(key, value)
+	}
 }
