b093f4593fd0299e3426704fd79fda7df762091a
Julianagagliano7 <julianagagliano1@gmail.com>
2025-07-21 15:30:53 -0300

    refactor: remove meet package and implement JSON marshaling/unmarshaling in main

--- main.go
+++ main.go
@@ -1,10 +1,42 @@
 package main
 
 import (
-	"github.com/Julianagagliano7/go-studies/meet"
+	"encoding/json"
+	"fmt"
 )
 
+type Request struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+type Pessoa struct {
+	Nome  string `json:"nome"`
+	Idade int    `json:"idade"`
+}
+
 func main() {
-	meet.SayHello()
-	meet.Say("Red Hot Chilli Peppers")
+
+	req := Request{
+		Name: "Juliana",
+		Age:  20,
+	}
+
+	payload, err := json.Marshal(req)
+	if err != nil {
+		fmt.Println("Erro ao fazer marshal: ", err)
+	}
+
+	fmt.Println("Resutado - Marshal: ", payload)
+
+	jsonString := `{"nome":"João","idade":30}`
+	var p Pessoa
+
+	err = json.Unmarshal([]byte(jsonString), &p)
+	if err != nil {
+		fmt.Println("Erro ao fazer unmarshal: ", err)
+		return
+	}
+	fmt.Println("Resultado Unmarshal: ", p)
+
 }
