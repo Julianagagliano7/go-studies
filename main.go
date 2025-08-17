5f7110b5ee889c2e974a6104ddd5a7d1c2c64aa7
Julianagagliano7 <julianagagliano1@gmail.com>
2025-08-17 20:58:03 -0300

    refactor: clean up main.go by removing unused code and organizing imports

--- main.go
+++ main.go
@@ -1,27 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-// & -> imprime o enderço
-// * -> imprime o conteúdo
-
-//Com ponteiros consigo modificar o valor original da variável
-
-type Pessoa struct {
-	Nome string
+func exibirMsg() {
+	fmt.Println("Olá de uma goroutine!")
 }
-
 func main() {
-	var p1 Pessoa = Pessoa{Nome: "Juliana"}
-	var p2 Pessoa = Pessoa{Nome: "Esmael"}
-	fmt.Println(p1)
-
-	var p3 *Pessoa = &p1
-	p3.Nome = "Joana"
-
-	// fmt.Println(&p1.Nome)
-	// fmt.Println(&p3.Nome)
-	
-	fmt.Println(p1)
-	fmt.Println(p2)
+	go exibirMsg()
+	time.Sleep(1 * time.Second)
+	fmt.Println("Olá main function!")
 }
