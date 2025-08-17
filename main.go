6b7f107ae9e2a84474c8057d8793278477db5f1b
Julianagagliano7 <julianagagliano1@gmail.com>
2025-08-17 19:41:25 -0300

    refactor: restructure Pessoa struct and implement Apresentar method

--- main.go
+++ main.go
@@ -2,12 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
-	result := soma(20, 67)
-
-	fmt.Println(result)
+type Pessoa struct {
+	Nome  string
+	Idade int
 }
 
-func soma (a, b int) int {
-	return a + b
+// Pessoa é uma struct que recebe o método Apresentar
+// Método apresentar fica exclusivamente relacionado à essa struct e pode modificar seus valores e comportamentos
+
+//func (p Pessoa) -> recebem uma cópia da struct (não a struct original), com isso, a struct original não é modificada
+//func (p *Pessoa) -> recebem um ponteiro da struct original, logo, ela é modificada
+
+//obs: remova o asterisco e rode para ver a diferença entre os cenários
+
+func (p *Pessoa) Apresentar() {
+	p.Nome = "Roberto" //modifica a struct original
+	fmt.Printf("Olá, meu nome é %s e tenho e tenho %d anos\n", p.Nome, p.Idade)
+}
+
+func main() {
+	p1 := Pessoa{Nome: "Juliana", Idade: 21}
+	p1.Apresentar()
+	fmt.Println(p1.Nome)
 }
