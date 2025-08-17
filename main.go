f193e14da656dbc0c282c607a600115a24352fc7
Julianagagliano7 <julianagagliano1@gmail.com>
2025-08-17 20:21:24 -0300

    pointers example

--- main.go
+++ main.go
@@ -2,26 +2,26 @@ package main
 
 import "fmt"
 
+// & -> imprime o enderço
+// * -> imprime o conteúdo
+
+//Com ponteiros consigo modificar o valor original da variável
+
 type Pessoa struct {
-	Nome  string
-	Idade int
-}
-
-// Pessoa é uma struct que recebe o método Apresentar
-// Método apresentar fica exclusivamente relacionado à essa struct e pode modificar seus valores e comportamentos
-
-//func (p Pessoa) -> recebem uma cópia da struct (não a struct original), com isso, a struct original não é modificada
-//func (p *Pessoa) -> recebem um ponteiro da struct original, logo, ela é modificada
-
-//obs: remova o asterisco e rode para ver a diferença entre os cenários
-
-func (p *Pessoa) Apresentar() {
-	p.Nome = "Roberto" //modifica a struct original
-	fmt.Printf("Olá, meu nome é %s e tenho e tenho %d anos\n", p.Nome, p.Idade)
+	Nome string
 }
 
 func main() {
-	p1 := Pessoa{Nome: "Juliana", Idade: 21}
-	p1.Apresentar()
-	fmt.Println(p1.Nome)
+	var p1 Pessoa = Pessoa{Nome: "Juliana"}
+	var p2 Pessoa = Pessoa{Nome: "Esmael"}
+	fmt.Println(p1)
+
+	var p3 *Pessoa = &p1
+	p3.Nome = "Joana"
+
+	// fmt.Println(&p1.Nome)
+	// fmt.Println(&p3.Nome)
+	
+	fmt.Println(p1)
+	fmt.Println(p2)
 }
