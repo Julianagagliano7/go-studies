eab4e44cab422288736781799c71a9a1dd5a6a27
Julianagagliano7 <julianagagliano1@gmail.com>
2025-09-14 17:35:05 -0300

    go routine order

--- main.go
+++ main.go
@@ -5,11 +5,23 @@ import (
 	"time"
 )
 
-func exibirMsg() {
-	fmt.Println("Olá de uma goroutine!")
+func sayHello() {
+	for i := 0; i < 3; i++ {
+		fmt.Println("Hello")
+		time.Sleep(100 * time.Millisecond)
+	}
 }
+
+
+func sayWorld() {
+	for i := 0; i < 3; i++ {
+		fmt.Println("World")
+		time.Sleep(150 * time.Millisecond)
+	}
+}
+
 func main() {
-	go exibirMsg()
-	time.Sleep(1 * time.Second)
-	fmt.Println("Olá main function!")
+	go sayHello()
+	go sayWorld()
+	time.Sleep(1 * time.Millisecond)
 }
